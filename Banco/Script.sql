02196b2414965420c5daf8f07277c5e8585bbd55
GustavoPisanello <99992149+GustavoPisanello@users.noreply.github.com>
2023-08-17 10:15:23 -0300

    Add files via upload

--- Banco/Script.sql
+++ Banco/Script.sql
@@ -13,25 +13,31 @@ create table tbIgrejaFav(
 
 create table tbUsuario(
 	IdUsu int primary key auto_increment,
-	UsuEmail char(50) not null,
+	UsuEmail char(50) not null unique,
     UsuSenha char(10) not null,
-    IgrejaFav int,
-    foreign key (IgrejaFav) references tbIgrejaFav(idIgreja)
+    IgrejaFav int
 );
 
-insert into tbIgrejaFav values(default,"Capela Nossa Senhora da Conceição", "Rua Nossa Senhora da Conceição", "06894822", "SP", "11865294568");
+alter table tbUsuario add constraint fkIgrejaFav foreign key (IgrejaFav) references tbIgrejaFav(idIgreja);
 
 
-create procedure spInsereIgreja (@NomeIgreja)
+delimiter $$
+create procedure spInsertUsuario(vEmail char(50), vSenha char(10), vIgreja char(50))
 begin
-set @IgrejaFavorita = (select idIgreja from tbIgrejaFav where NomeIgreja = @NomeIgreja);
-end
+	set @IgrejaFavorita = (select idIgreja from tbIgrejaFav where NomeIgreja = vIgreja);
+	insert into tbUsuario values(default, vEmail, vSenha, @IgrejaFavorita);
+end;
+$$
 
-insert into tbUsuario values(default, "pedrinho@gmail.com", "158575", @IgrejaFavorita);
+create procedure spSelectIgrejaFav(vEmail char(50))
+begin
+	Select NomeIgreja as "Igreja Favorita", RuaIgreja as "Endereço da Igreja", CEPIgreja as "CEP da Igreja", UFIgreja as "UF", TelIgreja as "Telefone" FROM tbUsuario inner join tbIgrejaFav where tbUsuario.IgrejaFav = tbIgrejaFav.idIgreja and UsuEmail = vEmail;
+end;
+$$
 
+delimiter ;
+insert into tbIgrejaFav values(default,"Capelo Nossa Senhora da Conceição", "Rua Nossa Senhora da Conceição", "06894822", "SP", "11865294568");
+call spInsertUsuario("pedrinhe.laurzite@gmail.com", "2345MEIA78", "Capelo Nossa Senhora da Conceição");
+call spSelectIgrejaFav("pedrinhe.laurzite@gmail.com");
 
-
-
-
-
-Select * from tbUsuario;
+select * from tbUsuario;
